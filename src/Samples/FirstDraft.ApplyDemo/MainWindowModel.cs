bee6f04baa62d2c4bc87c042f7f8101428048bf9
deamon-XiaoMi <271884460@qq.com>
2022-08-14 16:04:33 +0800

    1.TextBoxFly样式的更改 2.主页完善搜索导航页功能

--- src/Samples/FirstDraft.ApplyDemo/MainWindowModel.cs
+++ src/Samples/FirstDraft.ApplyDemo/MainWindowModel.cs
@@ -2,6 +2,8 @@ using FirstDraft;
 using MeiMvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 
@@ -12,36 +14,94 @@ namespace FirstDraft.ApplyDemo
 
         public string WindowsTitle { get; set; }
 
-        public List<NaviItem> Items { get; set; }
+     
+
+        public MainWindowModel()
+        {
+            NaviItems = new List<NaviItem>();
+            NaviItems.Add(new NaviItem() { Title = "欢迎 FirstDraft(F D)", Icon = ServiceProvider.Get<IconSet>().welcome, Content = new Views.WelcomeView() });
+            NaviItems.Add(new NaviItem() { Title = "按钮(Button)", Icon = ServiceProvider.Get<IconSet>().rect_fill, Content = new Views.ApplyButtonView() });
+            NaviItems.Add(new NaviItem() { Title = "图标按钮(IconButton)", Icon = ServiceProvider.Get<IconSet>().tag_fill, Content = new Views.ApplyIconButtonView() });
+            NaviItems.Add(new NaviItem() { Title = "切换按钮(ToggleButton)", Icon = ServiceProvider.Get<IconSet>().togglebutton, Content = new Views.ApplyToggleButtonView() });
+            NaviItems.Add(new NaviItem() { Title = "单选按钮(RadioButton)", Icon = ServiceProvider.Get<IconSet>().radiobutton, Content = new Views.ApplyRadioButtonView() });
+            NaviItems.Add(new NaviItem() { Title = "复选框(CheckBox)", Icon = ServiceProvider.Get<IconSet>().checkbox_fill, Content = new Views.ApplyCheckBoxView() });
+            NaviItems.Add(new NaviItem() { Title = "文本框(TextBox)", Icon = ServiceProvider.Get<IconSet>().textbox, Content = new Views.ApplyTextBoxView() });
+            NaviItems.Add(new NaviItem() { Title = "下拉选项框(ComboBox)", Icon = ServiceProvider.Get<IconSet>().combobox, Content = new Views.ApplyComboBoxView() });
+            NaviItems.Add(new NaviItem() { Title = "图标集(IconSet)", Icon = ServiceProvider.Get<IconSet>().tags_fill, Content = new Views.IconSetView() });
+
+            Current = NaviItems[0];
+            SearchText = "";
+            Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+
+            WindowsTitle = $"First Draft Apply {version.ToString()} 缓慢而坚定地前进";
+        }
+
+        /// <summary>
+        /// 所有的导航页面
+        /// </summary>
+        private List<NaviItem> NaviItems;
 
         private NaviItem current;
-
+        /// <summary>
+        /// 当前页面
+        /// </summary>
         public NaviItem Current
         {
             get { return current; }
             set { Set(ref current, value); }
         }
 
-        public MainWindowModel()
+        private string searchText;
+
+        /// <summary>
+        /// 检索匹配
+        /// </summary>
+        public string SearchText
         {
-            Items = new List<NaviItem>();
-            Items.Add(new NaviItem() { Title = "欢迎 FirstDraft(F D)",  Icon = ServiceProvider.Get<IconSet>().welcome, Content = new Views.WelcomeView() });
-            Items.Add(new NaviItem() { Title = "按钮(Button)", Icon = ServiceProvider.Get<IconSet>().rect_fill, Content = new Views.ApplyButtonView() });
-            Items.Add(new NaviItem() { Title = "图标按钮(IconButton)", Icon = ServiceProvider.Get<IconSet>().tag_fill, Content = new Views.ApplyIconButtonView() });
-            Items.Add(new NaviItem() { Title = "切换按钮(ToggleButton)", Icon = ServiceProvider.Get<IconSet>().togglebutton, Content = new Views.ApplyToggleButtonView() });
-            Items.Add(new NaviItem() { Title = "单选按钮(RadioButton)", Icon = ServiceProvider.Get<IconSet>().radiobutton, Content = new Views.ApplyRadioButtonView() }); 
-            Items.Add(new NaviItem() { Title = "复选框(CheckBox)", Icon = ServiceProvider.Get<IconSet>().checkbox_fill, Content = new Views.ApplyCheckBoxView() }); 
-            Items.Add(new NaviItem() { Title = "文本框(TextBox)", Icon = ServiceProvider.Get<IconSet>().textbox, Content = new Views.ApplyTextBoxView() }); 
-            Items.Add(new NaviItem() { Title = "下拉选项框(ComboBox)", Icon = ServiceProvider.Get<IconSet>().combobox, Content = new Views.ApplyComboBoxView() });
-            Items.Add(new NaviItem() { Title = "图标集(IconSet)", Icon = ServiceProvider.Get<IconSet>().tags_fill, Content = new Views.IconSetView() });
-
-            Current = Items[0];
-
-            Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-
-            WindowsTitle = $"First Draft Apply {version.ToString()}";
+            get { return searchText; }
+            set
+            {
+                searchText = value;
+                RaisePropertyChanged(nameof(SearchText));
+                OnSearchTextChanged();
+            }
         }
 
+        public RelayCommand SearchCommand => new RelayCommand(() =>
+         {
+             OnSearchTextChanged();
+         });
+
+        /// <summary>
+        /// 当搜索文本发生变化时，修改搜索匹配的导航页
+        /// </summary>
+        private void OnSearchTextChanged()
+        {
+            if (string.IsNullOrWhiteSpace(SearchText))
+            {
+                Items = new ObservableCollection<NaviItem>(NaviItems);
+                return;
+            }
+
+            // 根据输入字符，检索原始数据中匹配的图标，并更新界面的绑定数据源
+            Items = new ObservableCollection<NaviItem>(NaviItems.Where(t => t.Title.ToLowerInvariant().Contains(SearchText.ToLowerInvariant())));
+        }
+
+
+        private ObservableCollection<NaviItem> items;
+
+        /// <summary>
+        /// 当前搜索匹配的导航页
+        /// </summary>
+        public ObservableCollection<NaviItem> Items
+
+        {
+            get { return items; }
+            set
+            {
+                Set(ref items, value);
+            }
+        }
     }
 
 
