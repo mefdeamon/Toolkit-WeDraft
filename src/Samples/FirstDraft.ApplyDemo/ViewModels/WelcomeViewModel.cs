ec048c2e95d01c5d4bada5f661230781cd0eb30a
deamon-XiaoMi <271884460@qq.com>
2023-08-10 23:48:54 +0800

    功能添加：支持主题切换[明亮][幽暗]

--- src/Samples/FirstDraft.ApplyDemo/ViewModels/WelcomeViewModel.cs
+++ src/Samples/FirstDraft.ApplyDemo/ViewModels/WelcomeViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 
 namespace FirstDraft.ApplyDemo.ViewModels
 {
@@ -19,5 +20,29 @@ namespace FirstDraft.ApplyDemo.ViewModels
             string path = @"https://www.bilibili.com/video/BV1AA411J7vZ";
             System.Diagnostics.Process.Start("explorer.exe", path);
         });
+
+
+        private Uri theme;
+
+        public Uri Theme
+        {
+            get { return theme; }
+            set
+            {
+                SetProperty(ref theme, value);
+                var themeDict = new ResourceDictionary { Source = theme };
+                Application.Current.Resources.MergedDictionaries.Add(themeDict);
+            }
+        }
+
+        public Dictionary<string, Uri> Sources { get; set; }
+
+        public WelcomeViewModel()
+        {
+            Sources = new Dictionary<string, Uri>();
+            Sources.Add("明亮", new Uri("/FirstDraft;component/Themes/Ui.Light.xaml", UriKind.Relative));
+            Sources.Add("幽暗", new Uri("/FirstDraft;component/Themes/Ui.Dark.xaml", UriKind.Relative));
+            Theme = Sources["明亮"];
+        }
     }
 }
