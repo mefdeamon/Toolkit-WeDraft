924a0b75ed9bec2267bfcc110151cab79f65b762
deamon-XiaoMi <271884460@qq.com>
2023-10-22 23:15:28 +0800

    添加AnimatableBaseView可以左右动画的User Control默认控件

---
+++ src/FirstDraft/Controls/AnimatableBaseView.cs
@@ -0,0 +1,39 @@
+﻿using FirstDraft.AttachedProperties;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FirstDraft.Controls
+{
+    public class AnimatableBaseView : UserControl
+    {
+
+        public AnimationDirection LoadedAnimateDirection { get; set; } = AnimationDirection.FromLeft;
+
+        public AnimationDirection UnLoadAnimateDirection { get; set; } = AnimationDirection.ToRight;
+
+        public bool ShouldAnimatedOut { get; set; } = false;
+
+        public AnimatableBaseView()
+        {
+            Loaded += AnimatableBaseView_Loaded;
+        }
+
+        private async void AnimatableBaseView_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (ShouldAnimatedOut)
+                await AddAnimationOut();
+            else
+                await AddAnimationIn();
+        }
+
+        private async Task AddAnimationIn()
+        {
+            await this.SlideAndFadeInAsync(LoadedAnimateDirection);
+        }
+
+        private async Task AddAnimationOut()
+        {
+            await this.SlideAndFadeOutAsync(UnLoadAnimateDirection);
+        }
+    }
+}
--- src/Samples/FirstDraft.ApplyDemo/Views/ApplyButtonView.xaml.cs
+++ src/Samples/FirstDraft.ApplyDemo/Views/ApplyButtonView.xaml.cs
@@ -1,26 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using FirstDraft.Controls;
 
 namespace FirstDraft.ApplyDemo.Views
 {
     /// <summary>
     /// Interaction logic for ApplyButtonView.xaml
     /// </summary>
-    public partial class ApplyButtonView : UserControl
+    public partial class ApplyButtonView : AnimatableBaseView
     {
         public ApplyButtonView()
         {
             InitializeComponent();
+            LoadedAnimateDirection = AttachedProperties.AnimationDirection.FromRight;
         }
     }
 }
